fb09f93fdb0970cfc9d50dcdbe136ad909b75e4c
dhantoot <danvincenttagailo@gmail.com>
2023-11-11 10:44:25 +0800

    updated enable legacyHeaders in request limiter

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minute
 	max: 5, // Limit each IP to 5 requests per `window` (here, per 1 minute)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	legacyHeaders: true, // Disable the `X-RateLimit-*` headers
 })
 
 // apply middlewares for express
